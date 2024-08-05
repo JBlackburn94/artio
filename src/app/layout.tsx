2916fa5550e91a495151c50a7d4e6b0afc142fe6
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-08-05 15:03:07 +0100

    adds gsap and @gsap/react library as well as navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Archivo } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const archivo = Archivo({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={archivo.className}>{children}</body>
+      <body className={archivo.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav className="flex justify-between px-[2.5rem] py-[0.813rem]">
+        <div>link</div>
+        <div>logo</div>
+        <div>socials</div>
+      </nav>
+    </header>
+  );
+}
