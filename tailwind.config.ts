bf98830515f51c70b93f4a511578b83c8e23892c
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-08-05 14:40:05 +0100

    adds initial project files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        "optil": ["OPTILency"],
+      },
+      colors: {
+        artioBlack: "#2F332B",
+        artioWhite: "#F0EEEA",
+        artioRed: "#C53434",
       },
     },
   },
