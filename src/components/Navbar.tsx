c14cdd8db3c8840b075201309091f4bc53f7b6ba
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-08-05 16:11:07 +0100

    adds tailwind classes for mobile to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,17 +14,17 @@ export default function Navbar() {
   return (
     <header>
       <nav className="flex justify-between px-[40px] py-[13px] items-center">
-        <div className="flex items-center justify-start gap-[20px] w-1/3">
+        <div className="hidden lg:flex items-center justify-start gap-[20px] w-1/3">
           {navLinks.map(({ id, name, href }) => (
             <Link key={id} href={href}>
               {name}
             </Link>
           ))}
         </div>
-        <div className="flex items-center justify-center w-1/3">
+        <div className="flex items-center justify-start lg:justify-center w-1/2 lg:w-1/3">
           <Image src={navLogo} alt="Artio Logo" className="w-[70px] h-auto" />
         </div>
-        <div className="flex items-center justify-end gap-[20px] w-1/3">
+        <div className="hidden lg:flex items-center justify-end gap-[20px] w-1/3">
           <Link href={"#"}>
             <FaInstagram className="text-[30px]" />
           </Link>
