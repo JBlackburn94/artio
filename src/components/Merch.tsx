c7f96c5c7addfcdc2ab827d5a9c963c592160d70
Jason Blackburn <blackburnjason1994@gmail.com>
2025-09-28 11:03:28 +0100

    fix height bug on hero merch bundles and adds listen now button to the embeded vid

--- src/components/Merch.tsx
+++ src/components/Merch.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import merch from '/public/video.png';
+import Button from '@/components/Button';
 
 export default function Merch() {
     return (
@@ -9,9 +10,9 @@ export default function Merch() {
         >
             <h1 className="artio-font text-5xl max-md:text-3xl text-center my-5">The Devil You Know feat. Cody
                 Frost</h1>
-            <h2 className="artio-font text-3xl max-md:text-2xl">Out now</h2>
+            <Button href="https://ffm.to/artiothedevilyouknow" content="Listen Now"/>
             <iframe
-                className="w-[calc(100%-48px)] max-w-[1000px] aspect-video mx-auto bg-black"
+                className="w-[calc(100%-48px)] mt-10 max-w-[1000px] aspect-video mx-auto bg-black"
                 src="https://www.youtube.com/embed/UVC2QEprJUo?si=k6X-FWHNpTUHW2uv"
                 title="YouTube video player" frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
